6727f57736c5ee0ab2510d8b13c271051f21a728
Hanny Ramzy <ramzy@webimpian.com>
2020-05-30 03:25:55 +0800

    Code cleanup & enhance UI.

--- index.php
+++ index.php
@@ -12,98 +12,108 @@ $buyer_name = 'John Doe';
 $buyer_email = 'johndoe@example.com';
 $buyer_tel = '0168788787';
 $payment_gateway = 1;
-
 $api_url = $config['bayarcash_api_url'];
 $order_description = 'Pencil';
 $payment_form_id = md5($order_no . time()); # Safety features: Generate and assign a dynamic form ID in order to prevent any automation on the client-side.
-
-
 ?>
-<html>
 
+<html>
 <head>
 	<title>Bayarcash Checkout Example</title>
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<style type="text/css">
-		.span-error {
-			background-color: #f2dede;
-			border-color: #eed3d7;
-			color: #b94a48;
-		}
-	</style>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
-
 <body>
-<div id="loader" style="display: none;"></div>
-<div id="container" class="container">
-	<form id="<?php echo $payment_form_id ?>" name="frmPayment" method="POST" action="<?php echo $api_url ?>">
-		<div style="background-color: black; padding: 10px 20px 20px;">
-			<img src="https://webimpian.com/wp-content/themes/aplikasiniaga/images/logo-webimpian.png" alt="Web Impian Sdn. Bhd." width="200">
+<div id="container" class="container col-6 mt-4 mb-4">
+	<div class="card">
+		<div class="card-header">
+			Transaction Details
 		</div>
-		<br>
+		<div class="card-body">
+			<form id="<?php echo $payment_form_id ?>" method="POST" action="<?php echo $api_url ?>" class="mb-0 pb-0">
 
-		<span>Order No: </span>
-		<fieldset>
-			<input type="text" name="order_no" readonly="true" value="<?php echo $order_no ?>"/>
-			<span id="error_order_no" style="display: none;" class="span-error">Order No Required</span>
-		</fieldset>
+				<div class="card-text">
+					<div class="row">
+						<div class="col">
 
-		<span>Order Amount: </span>
-		<fieldset>
-			<input type="text" name="order_amount" readonly="true" value="<?php echo $order_amount ?>"/>
-			<span id="error_order_amount" style="display: none;" class="span-error">Order Amount Required</span>
-		</fieldset>
+							<!-- ID Number -->
+							<div class="form-group mb-2">
+								<label class="mb-1" for="order_no">
+									<b>ID Number</b>
+								</label>
+								<div>
+									<input type="text" name="order_no" id="order_no" class="form-control" value="<?php echo $order_no ?>" required>
+								</div>
+							</div>
 
-		<span>Order description: </span>
-		<fieldset>
-			<input type="text" name="order_description" readonly="true" value="<?php echo $order_description ?> "/>
-			<span id="error_order_description" style="display: none;" class="span-error">Order Description Required</span>
-		</fieldset>
+							<!-- Amount -->
+							<div class="form-group mb-2">
+								<label class="mb-1" for="order_amount">
+									<b>Amount</b>
+								</label>
+								<div>
+									<input type="text" name="order_amount" id="order_amount" class="form-control" value="<?php echo $order_amount ?>" required>
+								</div>
+							</div>
 
+							<!-- Description -->
+							<div class="form-group mb-2">
+								<label class="mb-1" for="order_description">
+									<b>Description</b>
+								</label>
+								<div>
+									<input type="text" name="order_description" id="order_description" class="form-control" value="<?php echo $order_description ?>" required>
+								</div>
+							</div>
 
-		<span>Buyer name: </span>
-		<fieldset>
-			<input type="text" name="buyer_name" readonly="true" value="<?php echo $buyer_name ?>"/>
-			<span id="error_buyer_name" style="display: none;" class="span-error">Buyer Name Required</span>
-		</fieldset>
+							<!-- Name -->
+							<div class="form-group mb-2">
+								<label class="mb-1" for="buyer_name">
+									<b>Name</b>
+								</label>
+								<div>
+									<input type="text" name="buyer_name" id="buyer_name" class="form-control" value="<?php echo $buyer_name ?>" required>
+								</div>
+							</div>
 
+							<!-- Email -->
+							<div class="form-group mb-2">
+								<label class="mb-1" for="buyer_email">
+									<b>Email</b>
+								</label>
+								<div>
+									<input type="text" name="buyer_email" id="buyer_email" class="form-control" value="<?php echo $buyer_email ?>" required>
+								</div>
+							</div>
 
-		<span>Buyer email: </span>
-		<fieldset>
-			<input type="email" name="buyer_email" readonly="true" value="<?php echo $buyer_email ?>"/>
-			<span id="error_buyer_email" style="display: none;" class="span-error">Buyer Email Required</span>
-		</fieldset>
+							<!-- Telephone -->
+							<div class="form-group mb-2">
+								<label class="mb-1" for="buyer_tel">
+									<b>Telephone</b>
+								</label>
+								<div>
+									<input type="text" name="buyer_tel" id="buyer_tel" class="form-control" value="<?php echo $buyer_tel ?>" required>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
+				<input type="hidden" name="payment_gateway" readonly="true" value="<?php echo $payment_gateway ?>"/>
+				<input type="hidden" name="return_url" readonly="true" value="<?php echo $return_url ?>"/>
+				<input type="hidden" name="api_url" readonly="true" value="<?php echo $api_url ?>"/>
+				<input type="hidden" name="portal_key" readonly="true" value="<?php echo $fpx_portal_key ?>"/>
 
-		<span>Buyer Telephone Number: </span>
-		<fieldset>
-			<input type="text" name="buyer_tel" readonly="true" value="<?php echo $buyer_tel ?>"/>
-			<span id="error_buyer_tel" style="display: none;" class="span-error">Buyer Telephone Number Required</span>
-		</fieldset>
-
-		<input type="hidden" name="payment_gateway" readonly="true" value="<?php echo $payment_gateway ?>"/>
-		<input type="hidden" name="return_url" readonly="true" value="<?php echo $return_url ?>"/>
-		<input type="hidden" name="api_url" readonly="true" value="<?php echo $api_url ?>"/>
-		<input type="hidden" name="portal_key" readonly="true" value="<?php echo $fpx_portal_key ?>"/>
-
-		<button id="checkoutButton" class="button-success pure-button button-large" type="button">Checkout</button>
-		<input type="submit" id="submitButton" style="display: none;"/>
-
-	</form>
-
+				<!-- Submit -->
+				<button class="btn btn-primary mt-2" type="submit">
+					Proceed to Payment
+				</button>
+			</form>
+		</div>
+	</div>
 </div>
 
+<!-- Footer -->
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<script type
-"text/javascript">
-window.onload=function() {
-jQuery("#checkoutButton").on('click', function() {
-jQuery('form[name="frmPayment"]').submit();
-})
-}
-</script>
-<
-/body>
-< /html>
+</body>
+</html>
