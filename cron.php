ab2219cd58b6f8cc4325082ce52182a497679c11
Afiq Hamzah <afiqhamzah@webimpian.com>
2020-07-15 12:25:44 +0800

    Update cron.php with guide comment

--- cron.php
+++ cron.php
@@ -1,7 +1,13 @@
 <?php
+require_once('config.php');
 
-$bearer_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiOTgzNWExNWYxMmE2ODI4YmU3ZmI3ZDE2ZDc5ZDI0MGRkMDE5YjE3MDVlOTI3NmM1MDQyNTZmNTQxOTBhYmY1YjkwZTU0NGQ5NTgxYzYzZDEiLCJpYXQiOjE1ODQxNTc4MzksIm5iZiI6MTU4NDE1NzgzOSwiZXhwIjoxNjE1NjkzODM5LCJzdWIiOiI1Iiwic2NvcGVzIjpbIioiXX0.xYE0NDMZe0lV4bl2mJ6MkQaq4nepEjY8iLzaBwHsrgJOwg-qMs0p-zrdsBijnVBaL5sOH6obvxVdph42_EohV1KYhCrhN1QmBub7ws7A2w8qheOIp8suZswlE380R3mTPyTrdmzWLjWazg8AVZ3m9PHwM3s-lvRXupSrK-JiAHxYlDmyHA0wDbkPRs3p3fAbcb7g6-lO_TcnI-a73Xk3mTroQC0z5SFnFfastDLGHFEPJkI8TjYkbjb4taar8rDUbhWWfBbe7EpRu0fG-cPAkE64CWnsd-G0G_a0PMmIhxjx5SJNJs8iIPZi4kNf9qkFH69i7RQAkc3jNXoPPkzMrz6lo21S1uXrjxKrC2BwlYmx80t-NeoR9cMCXQd-rVtVGOPkWKY_-gT9qSA9xDexT-bg_NEEHBBqU3pzkPfXByzQZEP7MN1hk02-AWN25XYL6TGSz6jU5jIMw8MhQqgNLnfmKrAM9VUNPZeMOFrWaMa6383FHpIOtnkBtad8NWNuWjr7BQVcASZAAyLjgkkkwD0NHX8tT-AjsWSSTq2U5i3uAlqtYsytgnz_e9NkjEZDF7h3uilCIuX5dqI7SCrw-9O4ad3vJNTVW5RKGU7vqiVyIsTygILJpkJj4nMDMoyBWu00dVqagcjhfzVaZIYQcKgipsK_az8-kTCA53j2gIw";
+$bearer_token = $config['bayarcash_FPX_portal_key'];
 
+/**
+* Query for order that have the status 'pending order' preferably for today only here,
+* extract the fpx order ref no in array and
+* assign to the variable $FPX_OrderRefNo below
+**/
 $FPX_OrderRefNo = [
     "1-593-263-118-994880",
     "1-594-106-159-975990",
