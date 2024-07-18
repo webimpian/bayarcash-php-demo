9a3425d27cdf270c9b47e0fd9d67c815ba4e042e
Hanny Ramzy <ramzy@webimpian.com>
2024-07-19 01:20:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -133,7 +133,7 @@ $payment_form_id = md5($order_no.time()); // Safety features: Generate and assig
 				<!--  Display list of payment channel buttons -->
 				<div class="row mt-2">
 					<?php foreach($payment_gateways as $id => $label) : ?>
-						<div class="col-12 button mb-3">
+						<div class="col-12 button mb-2">
 							<button type="submit" class="btn btn-success btn-block mr-1 h-100 p-2" onclick="$('#payment_gateway').val(<?php echo $id; ?>);">
 								<?php echo $label; ?>
 							</button>
